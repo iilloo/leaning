bca061a36fea0a7728678c719837ba076c679cf2
iilloo <wanghp5520jlu@outlook.com>
2023-05-19 15:41:25 +0000

    commit

--- test/test.c
+++ test/test.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
+
+long test(int n)
+{
+   
+    long result = 1;
+    long i = 1;
+    while(n-- > 2)
+    {
+        long j = result;
+        result +=  i;
+        i = j;
+    }
+    return result;
+}
 int main(void)
 {
-    printf("aaaaa\n");
+    printf("%ld\n",test(2));
     return 0 ;
 }
+
